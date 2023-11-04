66f14090d3899b8f63978fd60a8c7bd4fecf19a2
rofikul-resan <resan6231@gmail.com>
2023-11-04 22:36:51 +0600

    mongodb add

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const userRoute = require("./router/userRouter");
 const port = process.env.PORT || 5000;
 const app = express();
+require("dotenv").config();
 
 //middleware
 app.use(cors());
 app.use(express.json());
 app.use("/user", userRoute);
+
+mongoose.connect(`${process.env.SERVER_URL}/RR-hub`).then(() => {
+  console.log("db connect");
+});
 
 app.get("/", (req, res) => {
   res.send("server is running");
--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const userRoute = express.Router();
 
+userRoute.post("/createUser", async (req, res) => {
+  const data = req.body;
+});
+
 module.exports = userRoute;
