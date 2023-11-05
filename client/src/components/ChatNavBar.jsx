f1a25a9e3433144ccd8de0f2de3e11b4bbfd8d0a
rofikul-resan <resan6231@gmail.com>
2023-11-05 20:43:05 +0600

    add user search methode

--- client/src/components/ChatNavBar.jsx
+++ client/src/components/ChatNavBar.jsx
@@ -1,9 +1,24 @@
-import { Image, Input } from "@nextui-org/react";
+import { Avatar, Image, Input } from "@nextui-org/react";
 import { AiOutlineSearch } from "react-icons/ai";
 import UserInfo from "./UserInfo";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { nameShorter, serverUrl } from "../utils";
 
 const ChatNavBar = () => {
+  const [searchKey, setSearchKey] = useState("");
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      axios.get(`${serverUrl}/user/search?key=${searchKey}`).then((res) => {
+        console.log(res.data);
+        setUsers(res.data);
+      });
+    }, 500);
+  }, [searchKey]);
+
   const user = useSelector((s) => s.user);
   return (
     <div className=" py-3 px-3 space-y-3 ">
@@ -34,22 +49,29 @@ const ChatNavBar = () => {
         size="sm"
         startContent={<AiOutlineSearch className="text-white" />}
         type="search"
+        onChange={(e) => setSearchKey(e.target.value)}
       />
-      {/* <div className="relative">
-        <div className=" font-semibold absolute top-0 z-20 bg-black/95 w-full p-2 rounded-md  max-h-52 h-fit overflow-scroll ">
-          <div className="flex gap-3 text-xl items-center border-b pb-1  hover:translate-x-1 duration-150 cursor-pointer">
-            <Avatar
-              size="sm"
-              name={nameShorter("resan")}
-              isBordered
-              color="secondary"
-              className="w-5 h-5 m-1"
-            />
+      {users.length > 0 && (
+        <div className="relative">
+          <div className=" font-semibold absolute top-0 z-20 bg-black/95 w-full p-2 pb-4 rounded-md  max-h-52 h-fit overflow-scroll ">
+            {users.map((user) => (
+              <div
+                key={user._id}
+                className="flex gap-3 text-xl items-center border-b pb-1  hover:translate-x-1 duration-150 cursor-pointer"
+              >
+                <Avatar
+                  name={nameShorter(user?.name)}
+                  color="secondary"
+                  className="w-5 h-5 m-1 rounded-sm"
+                  src={user?.userPhoto}
+                />
 
-            <p className="capitalize text-sm">Resan</p>
+                <p className="capitalize text-sm">{user?.name}</p>
+              </div>
+            ))}
           </div>
         </div>
-      </div> */}
+      )}
     </div>
   );
 };
