2bdfb9c0a6fb8f07b4cd8941b79d0a4efa263edd
rofikul-resan <resan6231@gmail.com>
2023-11-06 01:58:38 +0600

    add send message api

--- server/router/messageRoute.js
+++ server/router/messageRoute.js
@@ -6,10 +6,10 @@ const msgRoute = express.Router();
 msgRoute.post("/msg", async (req, res) => {
   const members = req.body;
   const user1 = members[0];
-  const user2 = members[0];
+  const user2 = members[1];
   try {
     const isExistMessage = await Message.findOne({
-      $and: [{ "members._id": user1._id }, { "members._id": user1._id }],
+      $and: [{ "members._id": user1._id }, { "members._id": user2._id }],
     });
     console.log(isExistMessage);
     if (!isExistMessage) {
@@ -32,6 +32,43 @@ msgRoute.post("/msg", async (req, res) => {
     console.log(err);
     res.sendStatus(500);
   }
+});
+
+msgRoute.get("/msg/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const result = await Message.findById(id);
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+msgRoute.get("/user", async (req, res) => {
+  const userId = req.query.userId;
+  try {
+    const userMessage = await Message.find({
+      "members._id": userId,
+    }).select({ messages: 0 });
+    res.send(userMessage);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+msgRoute.put("/:id", async (req, res) => {
+  const msg = req.body;
+  const id = req.params.id;
+  console.log(msg);
+  const result = await Message.updateOne(
+    { _id: id },
+    {
+      $push: { messages: msg },
+    }
+  );
+  res.send(result);
 });
 
 module.exports = msgRoute;
